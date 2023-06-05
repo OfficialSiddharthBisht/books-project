4630450bafc650e6cf223594a1f4fdf3b77637f9
Siddharth <officialsiddharthbisht@gmail.com>
2023-06-05 14:09:17 +0530

    feat: header navbar

--- src/Layout/HeaderFooter/Header.tsx
+++ src/Layout/HeaderFooter/Header.tsx
@@ -1,14 +1,58 @@
-import React from 'react'
-import { ColorModeSwitcher } from "../../ColorModeSwitcher"
+import React from "react";
+import { MoonIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  Button,
+  Container,
+  IconButton,
+  useColorMode,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { FilterIcon, LightBulbIcon } from "../../styles/icons";
 
+const Header: React.FC = () => {
+  const bgColor = useColorModeValue("whitesmoke", "gray.500");
+  const { toggleColorMode } = useColorMode();
+  const themeIcon = useColorModeValue(<MoonIcon />, <LightBulbIcon />);
 
-const Header = () => {
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
   return (
-    <div>
-        <ColorModeSwitcher justifySelf="flex-end" />
-
-    </div>
-  )
-}
-
-export default Header
+    <Box position="fixed" w="100%" zIndex="1" backgroundColor={bgColor}>
+      <Container
+        maxW={"xl"}
+        py={4}
+        display={"flex"}
+        justifyContent={"space-between"}
+        alignItems={"center"}
+      >
+        <Button
+          variant={"ghost"}
+          fontSize={"xl"}
+          onClick={scrollToTop}
+        >
+          NCERT BOOKS
+        </Button>
+        <Box>
+          <IconButton
+            ml={1}
+            onClick={toggleColorMode}
+            variant="ghost"
+            aria-label="Toggle theme"
+            icon={themeIcon}
+          />
+          <IconButton
+            ml={1}
+            onClick={toggleColorMode}
+            variant="ghost"
+            aria-label="Toggle theme"
+            icon={themeIcon}
+          />
+        </Box>
+      </Container>
+    </Box>
+  );
+};
+export default Header;
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,8 +1,65 @@
+import { Box, Grid, GridItem } from '@chakra-ui/react'
 import React from 'react'
 
+const data = [
+  {
+    id:Date.now(),
+    standard : 1,
+  },
+  {
+    id:Date.now(),
+    standard : 1,
+  },
+  {
+    id:Date.now(),
+    standard : 1,
+  },
+  {
+    id:Date.now(),
+    standard : 1,
+  },
+  {
+    id:Date.now(),
+    standard : 1,
+  },
+  {
+    id:Date.now(),
+    standard : 1,
+  },
+  {
+    id:Date.now(),
+    standard : 1,
+  },
+  {
+    id:Date.now(),
+    standard : 1,
+  },
+  {
+    id:Date.now(),
+    standard : 1,
+  },
+  {
+    id:Date.now(),
+    standard : 1,
+  },
+  {
+    id:Date.now(),
+    standard : 1,
+  },
+  {
+    id:Date.now(),
+    standard : 1,
+  },
+]
 const Home: React.FC = () => {
   return (
-    <div>Home</div>
+    <Box>
+      <Grid templateColumns='repeat(3,1fr)' gap={6} mx={50} >
+        {data.map((book,index)=>(
+          <GridItem  w='100%' h='50' bg='gray.400' textAlign='center' fontSize='4xl'>{index + 1}</GridItem>
+        ))}
+      </Grid>
+    </Box>
   )
 }
 
